0852440849f419f9c3ab12cb27eda91ef799af2f
Freddy179 <130077054+Freddy179@users.noreply.github.com>
2023-09-20 18:05:06 +0300

    Add files via upload

---
+++ Практика номер 3.py
@@ -0,0 +1,26 @@
+# номер 1
+a = int(input())
+b = int(input())
+c = int(input())
+if 1<=a<=3 or 1<=b<=3 or 1<=c<=3:
+    if 1<=a<=3:
+        print('число', a, 'подходит')
+    if 1<=b<=3:
+        print('число', b, 'подходит')
+    if 1<=c<=3:
+        print('число', c, 'подходит')
+    else:
+        print('False')
+# # номер 7
+# a = int(input('Введите год: '))
+# if a % 4 == 0:
+#     print('True')
+# else:
+#     print('False')
+
+# номер 12
+# a = input()
+# if a[0] == '-':
+#     print('Число отрицательное')
+# else:
+#     print('Число положительное')
