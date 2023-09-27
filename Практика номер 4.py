13b4e7eb6816088339d036dd0621100a15e7423e
Freddy179 <130077054+Freddy179@users.noreply.github.com>
2023-09-27 17:06:57 +0300

    Add files via upload

---
+++ Практика номер 4.py
@@ -0,0 +1,22 @@
+# # номер 7
+# n = int(input())
+# z1 = 1
+# z2 = 0
+# for i in range(1, n+1):
+#     z1*=i
+#     z2+=z1
+# print(z2)
+
+# # номер 6
+# N = int(input())
+# z = 1
+# for i in range(1, N+1):
+#     z *= i
+# print(z)
+
+# # # номер 5
+# N = int(input())
+# z = 0
+# for i in range(1, N+1):
+#     z+=i**3
+# print(z)
